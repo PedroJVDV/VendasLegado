87ab9825b839de12eed0862d1bb11b7bd3fb375b
Pedro <scrowzer.graal2@gmail.com>
2026-01-12 14:18:07 -0300

    simplify the code fixing here, BaseTableCreation are here!

--- src/main/java/com/pedrojvdv/jdbc/creativeJDBC/create/tableCreation/data.sql
+++ src/main/java/com/pedrojvdv/jdbc/creativeJDBC/create/tableCreation/data.sql
@@ -3,4 +3,13 @@ CREATE TABLE IF NOT EXISTS products (
     name_prod        VARCHAR(150) NOT NULL,
     empressProd_name VARCHAR(60)  NOT NULL,
     quantity_prod    int
-)
+);
+
+CREATE TABLE IF NOT EXISTS users(
+    id    INT AUTO_INCREMENT PRIMARY KEY,
+    nome  VARCHAR(150) NOT NULL,
+    email VARCHAR(150) UNIQUE,
+    idade int
+
+);
+
