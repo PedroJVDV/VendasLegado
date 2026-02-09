b021cedc292cdd31c005e167549751ad0a4dc2db
Pedro <scrowzer.graal2@gmail.com>
2026-02-09 16:16:25 -0300

    products crud, new functions ++

--- src/main/java/com/pedrojvdv/jdbc/queries/ProductCrudQueries.java
+++ src/main/java/com/pedrojvdv/jdbc/queries/ProductCrudQueries.java
@@ -1,4 +1,31 @@
 package com.pedrojvdv.jdbc.queries;
 
 public class ProductCrudQueries {
+
+    public static final String INSERT_PRODUCT = """
+            INSERT INTO products(name, price, description, category, stock, available, created_at)
+            VALUES(?,?,?,?,?,?, CURRENT_TIMESTAMP)
+            """;
+
+    public static final String UPDATE_PRODUCTS = """
+            UPDATE products
+            SET name = COALESCE(?,name),
+                price = COALESCE(?, price),
+                description = COALESCE(?, description),
+                category = COALESCE(?, category),
+                stock = COALESCE(?, stock),
+                available = COALESCE(?, available)
+            WHERE id = ?
+            """;
+
+    public static final String DELETE_PRODUCT = """
+            DELETE FROM products
+            WHERE id = ?
+            """;
+
+    public static final String SOFT_DELETE_PRODUCT = """
+            UPDATE products
+            SET available = false
+            WHERE id = ?
+            """;
 }
