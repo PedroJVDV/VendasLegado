f74b04336f5c749596d25264bde5f552f8470334
Pedro <scrowzer.graal2@gmail.com>
2026-02-24 14:44:28 -0300

    new import

--- src/main/java/com/pedrojvdv/jdbc/dao/ProductDao.java
+++ src/main/java/com/pedrojvdv/jdbc/dao/ProductDao.java
@@ -5,6 +5,7 @@ import com.pedrojvdv.jdbc.queries.ProductQueries;
 
 import java.math.BigDecimal;
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
