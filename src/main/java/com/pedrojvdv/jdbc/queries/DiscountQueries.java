d3a8d6452b7da3c897dfd8a618c4f2dfba9879dc
Pedro <scrowzer.graal2@gmail.com>
2026-02-26 16:55:55 -0300

    flash_sale queries and discount calculation

--- src/main/java/com/pedrojvdv/jdbc/queries/DiscountQueries.java
+++ src/main/java/com/pedrojvdv/jdbc/queries/DiscountQueries.java
@@ -10,6 +10,7 @@ public class DiscountQueries {
             VALUES(?,?,?,CURRENT_DATE,?)
             """;
 
+    // TODO: separated functions by admin
     public static final String UPDATE_DISCOUNT = """
             UPDATE discount
             SET type = COALESCE(?, type), percentage = COALESCE(?, percentage), duration_hours = COALESCE(?, duration_hours)
@@ -41,10 +42,12 @@ public class DiscountQueries {
 
     //queries ++
     // TODO: comments @++
+    // TODO: separate by customer functions
     public static final String FIND_ALL_ACTIVE_DISCOUNTS = """
             SELECT
                 p.name AS product_name,
                 p.price AS product_price,
+                p.price * (1 - d.percentage / 100) AS final_price
                 d.percentage,
                 d.type,
                 d.start_date,
@@ -58,6 +61,43 @@ public class DiscountQueries {
             WHERE d.active = TRUE
             """;
 
+
+    //admin function
+    public static final String FIND_FLASH_SALES_ADMIN = """
+            SELECT
+                p.name AS product_name,
+                p.price AS product_price,
+                p.price * (1 - d.percentage / 100) AS final_price
+                d.percentage,
+                d.type,
+                d.start_date,
+                d.end_date,
+                d.duration_hours,
+                d.active,
+                d.created_at
+            FROM discount d
+            LEFT JOIN products p
+                ON d.product_id = p.id
+            WHERE d.type = 'FLASH_SALE'
+            AND d.active = TRUE
+            AND p.available = TRUE
+            ORDER BY d.created_at DESC
+            """;
+
+    public static final String FIND_FLASH_SALES_CUSTOMER = """
+            SELECT
+                p.name AS product_name,
+                p.price AS product_price,
+                p.price * (1 - d.percentage / 100) AS final_price
+                d.percentage,
+                d.duration_hours
+            FROM discount d
+            LEFT JOIN products p
+                ON d.product_id = p.id
+            WHERE d.type = 'FLASH_SALE'
+            AND d.active = TRUE
+            AND p.available = TRUE
+            """;
     public static final String FIND_ACTIVE_DISCOUNTS_BY_ID = """
             SELECT
                 d.id,
@@ -71,6 +111,7 @@ public class DiscountQueries {
                 d.created_at,
                 p.name AS product_name,
                 p.price AS product_price
+                p.price * (1 - d.percentage / 100) AS final_price
             FROM discount d
             LEFT JOIN products p
                 ON d.product_id = p.id
@@ -78,8 +119,7 @@ public class DiscountQueries {
             AND d.active = TRUE
             """;
 
-
-    public static final String FIND_EXPIRED_DISCOUNTS= """
+    public static final String FIND_EXPIRED_DISCOUNTS = """
             SELECT
                 d.id,
                 d.product_id,
@@ -91,13 +131,15 @@ public class DiscountQueries {
                 d.active,
                 d.created_at,
                 p.name AS product_name,
-                p.price AS product_price
+                p.price AS product_price,
+                p.price * (1 - d.percentage / 100) AS final_price
             FROM discount d
             LEFT JOIN products p
                 ON d.product_id = p.id
             WHERE d.end_date NOT NULL
             AND d.end_date < CURRENT_DATE
             """;
-    // TODO: find over discounts
+
+    //TODO: enum queries ??
 
 }
