fc79b85696b9bac2745bf9c4e63f5876642f23fd
Pedro <scrowzer.graal2@gmail.com>
2026-03-04 15:22:19 -0300

    new structure for userDao -> -185 lines -> 90 lines - new optimization

--- src/main/java/com/pedrojvdv/jdbc/dao/UserDao.java
+++ src/main/java/com/pedrojvdv/jdbc/dao/UserDao.java
@@ -1,258 +1,81 @@
 package com.pedrojvdv.jdbc.dao;
 
-import com.pedrojvdv.jdbc.config.ConnectionFactory;
-import com.pedrojvdv.jdbc.model.Users;
-import com.pedrojvdv.jdbc.validation.UserValidation;
+import com.pedrojvdv.jdbc.model.User;
+import com.pedrojvdv.jdbc.queries.UserCrudQueries;
+import com.pedrojvdv.jdbc.queries.UserQueries;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-public class UserDAO extends Users {
+public class UserDao{
 
-    public UserDAO(int id, String email, String nome) {
-        super(id, email, nome);
+    private final Connection connection;
+
+    public UserDao(Connection connection) {
+        this.connection = connection;
     }
 
-    // here they create, that's it
-    public void createUser() {
-        try {
+    public void createUser(User user) throws SQLException {
+        String sql = UserCrudQueries.CREATE_USER;
 
-            Scanner scanner = new Scanner(System.in);
-
-            System.out.println("Informe o seu nome de usuario: ");
-            setNome(scanner.nextLine());
-
-            System.out.println("Informe o seu Email de acesso: ");
-            setEmail(scanner.nextLine());
-
-            System.out.println("Informe sua Idade: ");
-            setIdade(scanner.nextInt());
-
-        } catch (Exception e) {
-            System.out.println("Não foi possível definir usuario!");
-        }
-
-    }
-
-    // insert users into database, nothing special here!
-    public void insertUser() {
-        try {
-
-            Users users = new Users(getId(), getEmail(), getNome());
-            users.setIdade(getIdade());
-
-            List<String> errorsValidate = UserValidation.listValidate(users);
-            if (!errorsValidate.isEmpty()) {
-                System.out.println("Erros de validação encontrados. Digite os dados corretamente!");
-                return;
-            }
-
-            if (!emailExists(users.getEmail())) {
-                System.out.println("Usuario incluido com sucesso!");
-            } else {
-                emailExists(users.getEmail());
-                {
-                    System.out.println("Email já cadastrado. Tente novamente!");
-                }
-            }
-
-            String sql = "INSERT INTO usuarios (nome, email, idade) VALUES (?, ?, ?)";
-
-
-            ParamsDAO paramsDAO = new ParamsDAO();
-            int id = paramsDAO.insertParam(sql, getNome(), getEmail(), getIdade());
-            System.out.println("ID gerado: " + id);
-
-        } catch (Exception e) {
-            System.out.println("Não foi possível criar usuario! " + e.getMessage());
+        try(PreparedStatement stmt = connection.prepareStatement(sql)){
+            stmt.setString(1, user.getName());
+            stmt.setString(2, user.getEmail());
+            stmt.setObject(3, user.getAge());
+            stmt.executeUpdate();
         }
     }
 
-    // list all user on database, just id, email and name;
-    public void listUsers() {
+    public void updateUser(User user) throws SQLException{
+        String sql = UserCrudQueries.UPDATE_USER;
 
-        String sql = "SELECT * FROM usuarios";
-        List<Users> usuarios = new ArrayList<>();
-
-        try (java.sql.Connection connection = ConnectionFactory.getConnection();
-             PreparedStatement stmt = connection.prepareStatement(sql);
-             ResultSet ResultSet = stmt.executeQuery()) {
-
-            // now, all the users in a query has inside a list
-            // * somente id, email e nome!
-            while (ResultSet.next()) {
-                Users usuario = new Users(ResultSet.getInt("id")
-                        , ResultSet.getString("email")
-                        , ResultSet.getString("nome"));
-                usuarios.add(usuario);
-            }
-
-            //show the SQL command line on console
-            for (Users user : usuarios) {
-                System.out.println("ID: " + user.getId() +
-                        " | Nome: " + user.getNome() +
-                        " | Email: " + user.getEmail());
-            }
-        } catch (Exception e) {
-            System.out.println("Não foi possivel listar usuarios! " + e.getMessage());
+        try(PreparedStatement stmt = connection.prepareStatement(sql)){
+            stmt.setObject(1, user.getName());
+            stmt.setObject(2, user.getEmail());
+            stmt.setObject(3, user.getAge());
+            stmt.setLong(4, user.getId());
+            stmt.executeUpdate();
         }
     }
 
-    // search the users by ID
-    public void idSearch() {
+    public void deleteUser(User user) throws SQLException{
+        String sql = UserCrudQueries.DELETE_USER;
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Digite o ID que deseja buscar: ");
-        int id = scanner.nextInt();
-        scanner.nextLine();
-
-        String sql = "SELECT * FROM usuarios WHERE id = ?";
-
-        try (java.sql.Connection connection = ConnectionFactory.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setInt(1, id);
-
-            ResultSet resultSet = statement.executeQuery();
-
-            if (resultSet.next()) {
-                System.out.println("Usuário localizado!");
-                System.out.println("-----");
-                System.out.println(" |ID: " + resultSet.getInt("id"));
-                System.out.println(" | Nome: " + resultSet.getString("nome"));
-                System.out.println(" | Email: " + resultSet.getString("email"));
-                System.out.println("-----");
-                resultSet.getInt("id");
-                resultSet.getString("email");
-                resultSet.getString("nome");
-
-            } else {
-                System.out.println("-----");
-                System.out.println("Usuario não foi encontrado!");
-                System.out.println("-----");
-
-            }
-
-        } catch (Exception e) {
-            System.out.println("-----");
-            System.out.println("Erro inesperado ao encontrar usuario:" + e.getMessage());
-            System.out.println("-----");
-        }
-    }
-    
-    // this layer is supposed to modify any user what you want, if they ID don't exist, nothing happens; this code layers change with coalesce command!
-    public void updateUser(Scanner scanner) {
-        
-        try {
-            System.out.println("Digite o ID do usuario que deseja fazer alterações: ");
-            int id = scanner.nextInt();
-            scanner.nextLine();
-
-            if (id <= 0) {
-                System.out.println("Digite um ID existente!");
-                return;
-            }
-
-            String novoNome = null;
-            String novoEmail = null;
-            Integer novaIdade = null;
-
-            System.out.println("Deseja alterar o nome de usuario? (S/N) ");
-            String respostaNome = scanner.nextLine();
-            if (respostaNome.equalsIgnoreCase("S")) {
-                System.out.println("Digite o novo nome de usuario: ");
-                novoNome = scanner.nextLine();
-            }
-
-            System.out.println("Deseja alterar o email do usuario? (S/N)");
-            String respostaEmail = scanner.nextLine();
-            if (respostaEmail.equalsIgnoreCase("S")) {
-                System.out.println("Digite o novo email: ");
-                novoEmail = scanner.nextLine();
-            }
-
-            System.out.println("Deseja alterar a idade? (S/N)");
-            String respostaIdade = scanner.nextLine();
-            if (respostaIdade.equalsIgnoreCase("S")) {
-                System.out.println("Digite a nova idade: ");
-                novaIdade = scanner.nextInt();
-            }
-
-            Users temp = new Users(id,
-                    novoEmail != null ? novoEmail : getEmail(),
-                    novoNome != null ? novoNome : getNome());
-            if (novaIdade != null) temp.setIdade(novaIdade);
-
-            List<String> errors = UserValidation.listValidate(temp);
-            if (!errors.isEmpty()) {
-                System.out.println("Erros de validação:");
-                for (String err : errors) System.out.println(" - " + err);
-                return;
-            }
-
-            // COALESCE --> se o valor do parametro for nulo, ou seja, não for inserido, o objeto permanecera igual! --> this affirmation are not real, I think --> change this later!
-            String sql = "UPDATE usuarios SET " +
-                    "nome = COALESCE(?, nome)," +
-                    "email = COALESCE(?, email)," +
-                    "idade = COALESCE(?, idade)" +
-                    "WHERE id = ?";
-
-            ParamsDAO paramsDAO = new ParamsDAO();
-            paramsDAO.updateParam(sql, novoNome, novoEmail, novaIdade, id);
-
-            System.out.println("-----");
-            System.out.println("Dados atualizados com sucesso!!");
-            System.out.println("-----");
-
-        } catch (Exception e) {
-            System.out.println("-----");
-            System.out.println("Erro ao atualizar usuario!" + e.getMessage());
-            System.out.println("-----");
+        try(PreparedStatement stmt = connection.prepareStatement(sql)){
+            stmt.setLong(1, user.getId());
+            stmt.executeUpdate();
         }
     }
 
+    public List<User> findUserByEmail(String email)throws SQLException{
+       return executeQuery(UserQueries.FIND_USER_BY_EMAIL, email);
+    }
 
-    // chose the user who will be deleted, this layer has a confirmation to delete and a try-catch to errors!
-    public void deleteUser() {
-        
-        try {
-            Scanner scanner = new Scanner(System.in);
+    public List<User> findUserById(Long id)throws SQLException{
+        return executeQuery(UserQueries.FIND_USER_BY_ID, id);
+    }
 
-            System.out.println("Informe o ID do usuario que deseja DELETAR: ");
-            int id = scanner.nextInt();
-            scanner.nextLine();
+    public List<User> findUserByUsername(String name)throws SQLException{
+        return executeQuery(UserQueries.FIND_USER_BY_USERNAME, name);
+    }
 
-            if (id <= 0) {
-                System.out.println("Digite um ID válido!");
-                return;
+    private List<User> executeQuery(String sql, Object... args)throws SQLException{
+        List<User> userList = new ArrayList<>();
+
+        try(PreparedStatement stmt = connection.prepareStatement(sql)){
+            for (int i = 0; i < args.length ; i++) {
+                stmt.setObject(i+1,args[i] );
             }
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                User user = mapResultToUser(rs);
+                userList.add(user);
 
-            System.out.println("Você realmente deseja DELETAR este usuario? (S/N)");
-            String respostaDelete = scanner.nextLine();
-            if (respostaDelete.equalsIgnoreCase("S")) {
-                String sql = "DELETE FROM usuarios WHERE id = ?";
-
-                ParamsDAO paramsDAO = new ParamsDAO();
-                paramsDAO.deleteParam(sql, id);
-
-                System.out.println("-----");
-                System.out.println("O usuario foi deletado com sucesso!");
-                System.out.println("-----");
-
-            } else {
-                System.out.println("-----");
-                System.out.println("Operação cancelada!");
-                System.out.println("-----");
             }
-
-        } catch (Exception e) {
-            System.out.println("-----");
-            System.out.println("Não foi possível deletar esse usuário!!" + e.getMessage());
-            System.out.println("-----");
+            return userList;
         }
+    }
 
     private User mapResultToUser(ResultSet rs) throws SQLException{
         User user = new User();
@@ -263,23 +86,4 @@ public class UserDAO extends Users {
         user.setCreatedAt(rs.getTimestamp("created_at").toLocalDateTime());
         return user;
     }
-
-    // if the email exists they don't do anything, else, they will redirect to the database! if exists, they don't add!!!
-    public boolean emailExists(String email) {
-
-        String sql = "SELECT 1 FROM usuarios WHERE email = ? LIMIT 1";
-        try (java.sql.Connection conn = ConnectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
-            ps.setString(1, email);
-            try (ResultSet rs = ps.executeQuery()) {
-                return rs.next();
-            }
-        } catch (Exception e) {
-            System.out.println("Aviso: falha ao verificar unicidade de email: " + e.getMessage());
-            return false;
-        }
-    }
-
 }
-
-
