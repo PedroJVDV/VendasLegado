54af5dec7dc7d86e14075db481036ad9b9beb94a
Pedro <scrowzer.graal2@gmail.com>
2026-03-05 20:31:18 -0300

    productValidation -> make the exceptions easy to resolve

--- src/main/java/com/pedrojvdv/jdbc/validation/ProductValidation.java
+++ src/main/java/com/pedrojvdv/jdbc/validation/ProductValidation.java
@@ -2,4 +2,42 @@ package com.pedrojvdv.jdbc.validation;
 
 public class ProductValidation {
 
+    public void validate(Product product){
+
+        validateName(product.getName());
+        validateCategory(product.getCategory());
+        validateDescription(product.getDescription());
+        validateStock(product.getStock(), product.getDiscountType());
+        validateAvailable(product);
+    }
+
+    private void validateAvailable(Product product) {
+        product.setAvailable(product.getStock() >= 1);
+    }
+
+    private void validateStock(Integer stock, DiscountType discountType) {
+        if (stock > 300){
+            throw new ProductValidationException("Stock não pode ultrapassar 300 unidades!");
+        }
+        if (stock > 100 && discountType != DiscountType.PERMANENT){
+            throw new ProductValidationException("Stock acima de 100 unidades só é permitido em DESCONTOS PERMANENTES!");
+        }
+    }
+    
+    private void validateDescription(String description) {
+        if (description == null || description.isBlank()){
+            throw new ProductValidationException("Descrição do produto não pode ser nula/vazia!");
+        }
+    }
+    private void validateCategory(Category category) {
+        if (category == null){
+            throw new ProductValidationException("Categoria não pode ser nula/vazia!");
+        }
+    }
+    private void validateName(String name) {
+        if(name == null || name.isBlank()){
+            throw new ProductValidationException("Nome não pode ser nulo/vazio");
+        }
+    }
+
 }
