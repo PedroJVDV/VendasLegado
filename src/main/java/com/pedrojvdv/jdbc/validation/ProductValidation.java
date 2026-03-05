5dfb6a92def2158ccb5ad6c9c895fbbaa781a615
Pedro <scrowzer.graal2@gmail.com>
2026-03-05 20:31:38 -0300

    new imports...

--- src/main/java/com/pedrojvdv/jdbc/validation/ProductValidation.java
+++ src/main/java/com/pedrojvdv/jdbc/validation/ProductValidation.java
@@ -1,5 +1,9 @@
 package com.pedrojvdv.jdbc.validation;
 
+import com.pedrojvdv.jdbc.exception.ProductValidationException;
+import com.pedrojvdv.jdbc.model.Category;
+import com.pedrojvdv.jdbc.model.DiscountType;
+import com.pedrojvdv.jdbc.model.Product;
 public class ProductValidation {
 
     public void validate(Product product){
@@ -23,7 +27,7 @@ public class ProductValidation {
             throw new ProductValidationException("Stock acima de 100 unidades só é permitido em DESCONTOS PERMANENTES!");
         }
     }
-    
+
     private void validateDescription(String description) {
         if (description == null || description.isBlank()){
             throw new ProductValidationException("Descrição do produto não pode ser nula/vazia!");
