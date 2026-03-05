9572b3a8319a7ad8e86a2d586f0949a329a7e2c2
Pedro <scrowzer.graal2@gmail.com>
2026-03-05 20:35:08 -0300

    discountException -> easy to search the problem --> specifically exceptions

---
+++ src/main/java/com/pedrojvdv/jdbc/exception/DiscountValidationException.java
@@ -0,0 +1,7 @@
+package com.pedrojvdv.jdbc.exception;
+
+public class DiscountValidationException extends RuntimeException {
+    public DiscountValidationException(String message) {
+        super(message);
+    }
+}
