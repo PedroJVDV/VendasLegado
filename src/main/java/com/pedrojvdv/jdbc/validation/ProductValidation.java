95b8958382bb82acdc9a57282929837f3ebd391f
Pedro <scrowzer.graal2@gmail.com>
2026-03-11 18:31:25 -0300

    validate available fixed

--- src/main/java/com/pedrojvdv/jdbc/validation/ProductValidation.java
+++ src/main/java/com/pedrojvdv/jdbc/validation/ProductValidation.java
@@ -4,6 +4,7 @@ import com.pedrojvdv.jdbc.exception.ProductValidationException;
 import com.pedrojvdv.jdbc.model.Category;
 import com.pedrojvdv.jdbc.model.DiscountType;
 import com.pedrojvdv.jdbc.model.Product;
+
 public class ProductValidation {
 
     public void validate(Product product){
@@ -11,23 +12,22 @@ public class ProductValidation {
         validateName(product.getName());
         validateCategory(product.getCategory());
         validateDescription(product.getDescription());
-        validateStock(product.getStock(), product.getDiscountType());
+        validateStock(product);
         validateAvailable(product);
     }
-
     private void validateAvailable(Product product) {
-        product.setAvailable(product.getStock() >= 1);
+       if (!product.getAvailable()){
+           throw new ProductValidationException("Produto não está disponível!");
+       }
     }
-
-    private void validateStock(Integer stock, DiscountType discountType) {
-        if (stock > 300){
+    private void validateStock(Product product) {
+        if (product.getStock() > 300){
             throw new ProductValidationException("Stock não pode ultrapassar 300 unidades!");
         }
-        if (stock > 100 && discountType != DiscountType.PERMANENT){
+        if (product.getStock() > 100 && product.getDiscountType() != DiscountType.PERMANENT){
             throw new ProductValidationException("Stock acima de 100 unidades só é permitido em DESCONTOS PERMANENTES!");
         }
     }
-
     private void validateDescription(String description) {
         if (description == null || description.isBlank()){
             throw new ProductValidationException("Descrição do produto não pode ser nula/vazia!");
