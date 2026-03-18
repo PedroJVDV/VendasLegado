fe526cda9ae15451305f4ae3cd41c7bf8e864c80
Pedro <scrowzer.graal2@gmail.com>
2026-03-17 21:05:30 -0300

    some bugs fixed + new structure

--- src/test/java/ProductFindTest.java
+++ src/test/java/ProductFindTest.java
@@ -1,117 +1,229 @@
 import com.pedrojvdv.jdbc.dao.ProductDao;
 import com.pedrojvdv.jdbc.model.Category;
 import com.pedrojvdv.jdbc.model.Product;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.quality.Strictness;
 
 import java.math.BigDecimal;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ProductFindTest {
 
-    @Mock
+    @Mock(strictness = Mock.Strictness.LENIENT)
+    private Connection connection;
+
     private ProductDao productDao;
+
+
+    @BeforeEach
+    void beforeSetUp() {
+        connection = mock(Connection.class);
+        productDao = new ProductDao(connection);
+    }
 
     @Test
     void findAllProductsTest() throws SQLException {
+        PreparedStatement stmt = mock(PreparedStatement.class);
+        ResultSet rs = mock(ResultSet.class);
 
-        List<Product> productTest = List.of(new Product("Cama", new BigDecimal("1249.99"), 25, true, "teste", Category.TESTE));
+        when(connection.prepareStatement(anyString())).thenReturn(stmt);
+        when(stmt.executeQuery()).thenReturn(rs);
 
-        when(productDao.findAllProducts()).thenReturn(productTest);
+        when(rs.next()).thenReturn(true).thenReturn(false);
+        when(rs.getLong("id")).thenReturn(1L);
+        when(rs.getString("name")).thenReturn("teste");
+        when(rs.getBigDecimal("price")).thenReturn(new BigDecimal("1249.99"));
+        when(rs.getInt("stock")).thenReturn(25);
+        when(rs.getBoolean("available")).thenReturn(true);
+        when(rs.getString("description")).thenReturn("teste");
+        when(rs.getString("category")).thenReturn(Category.TESTE.name());
+        when(rs.getTimestamp("created_at")).thenReturn(Timestamp.valueOf(LocalDateTime.now()));
+        when(rs.getBigDecimal("discount_percentage")).thenReturn(new BigDecimal("0.10"));
+        when(rs.getBigDecimal("final_price")).thenReturn(new BigDecimal("1124.99"));
 
         List<Product> result = productDao.findAllProducts();
 
+        verify(stmt).executeQuery();
+
         assertEquals(1, result.size());
-        assertEquals("Cama", result.get(0).getName());
+        assertEquals("teste", result.get(0).getName());
         assertEquals("teste", result.get(0).getDescription());
         assertEquals(Category.TESTE, result.get(0).getCategory());
     }
 
     @Test
     void findByIdTest() throws SQLException {
+        PreparedStatement stmt = mock(PreparedStatement.class);
+        ResultSet rs = mock(ResultSet.class);
 
-        Product productTest = new Product("botas de chuva", new BigDecimal("50.00"), 21, true, "teste", Category.TESTE);
+        when(connection.prepareStatement(anyString())).thenReturn(stmt);
+        when(stmt.executeQuery()).thenReturn(rs);
 
-        when(productDao.findById(1L)).thenReturn(productTest);
+        when(rs.next()).thenReturn(true).thenReturn(false);
+        when(rs.getLong("id")).thenReturn(1L);
+        when(rs.getString("name")).thenReturn("teste");
+        when(rs.getBigDecimal("price")).thenReturn(new BigDecimal("1249.99"));
+        when(rs.getInt("stock")).thenReturn(25);
+        when(rs.getBoolean("available")).thenReturn(true);
+        when(rs.getString("description")).thenReturn("teste");
+        when(rs.getString("category")).thenReturn(Category.TESTE.name());
+        when(rs.getTimestamp("created_at")).thenReturn(Timestamp.valueOf(LocalDateTime.now()));
+        when(rs.getBigDecimal("discount_percentage")).thenReturn(new BigDecimal("0.10"));
+        when(rs.getBigDecimal("final_price")).thenReturn(new BigDecimal("1124.99"));
 
         Product result = productDao.findById(1L);
 
-        assertEquals("botas de chuva", result.getName());
-
+        assertEquals("teste", result.getName());
     }
 
     @Test
     void findByCategory() throws SQLException {
+        PreparedStatement stmt = mock(PreparedStatement.class);
+        ResultSet rs = mock(ResultSet.class, withSettings().strictness(Strictness.LENIENT));
 
-        List<Product> productTest = List.of(
-                new Product("celta 1.0 1999", new BigDecimal("22399.99"), 1, true, "teste", Category.TESTE),
-                new Product("Corsa 1.4 2007", new BigDecimal("32450.00"), 1, true, "teste", (Category.TESTE)));
+        when(connection.prepareStatement(anyString())).thenReturn(stmt);
+        when(stmt.executeQuery()).thenReturn(rs);
 
-        when(productDao.findProductByCategory(Category.TESTE)).thenReturn(productTest);
+        when(rs.findColumn("name")).thenReturn(1);
+        when(rs.findColumn("price")).thenReturn(2);
+        when(rs.findColumn("category")).thenReturn(3);
+        when(rs.findColumn("description")).thenReturn(4);
+        when(rs.findColumn("stock")).thenReturn(5);
+        when(rs.findColumn("discount_percentage")).thenReturn(6);
+        when(rs.findColumn("final_price")).thenReturn(7);
+
+        when(rs.next()).thenReturn(true).thenReturn(false);
+
+        when(rs.getString("name")).thenReturn("teste");
+        when(rs.getBigDecimal("price")).thenReturn(new BigDecimal("1249.99"));
+        when(rs.getInt("stock")).thenReturn(25);
+        when(rs.getString("description")).thenReturn("teste");
+        when(rs.getString("category")).thenReturn(Category.TESTE.name());
+        when(rs.getBigDecimal("discount_percentage")).thenReturn(new BigDecimal("10.00"));
+        when(rs.getBigDecimal("final_price")).thenReturn(new BigDecimal("1124.99"));
+
+        when(rs.findColumn("created_at")).thenThrow(SQLClientInfoException.class);
 
         List<Product> resultList = productDao.findProductByCategory(Category.TESTE);
 
-        assertEquals(2, resultList.size());
-        assertEquals("celta 1.0 1999", resultList.get(0).getName());
-        assertEquals("teste", resultList.get(0).getDescription());
+        assertEquals(1, resultList.size());
+        assertEquals("teste", resultList.get(0).getName());
     }
 
     @Test
     void findWithDiscount() throws SQLException {
+        PreparedStatement stmt = mock(PreparedStatement.class);
+        ResultSet rs = mock(ResultSet.class, withSettings().strictness(Strictness.LENIENT));
 
-        List<Product> productTest = List.of(
-                new Product("camiseta seleção brasileira masculina - 2026", new BigDecimal("500.00"), 10, true, "teste", Category.TESTE),
-                new Product("camiseta seleção brasileira feminina - 2026", new BigDecimal("500.00"), 10, true, "teste", Category.TESTE));
+        when(connection.prepareStatement(anyString())).thenReturn(stmt);
+        when(stmt.executeQuery()).thenReturn(rs);
 
-        when(productDao.findProductWithDiscount()).thenReturn(productTest);
+        when(rs.findColumn("name")).thenReturn(1);
+        when(rs.findColumn("price")).thenReturn(2);
+        when(rs.findColumn("available")).thenReturn(3);
+        when(rs.findColumn("discount_percentage")).thenReturn(4);
+        when(rs.findColumn("price")).thenReturn(5);
+
+        when(rs.next()).thenReturn(true).thenReturn(false);
+
+        when(rs.getString("name")).thenReturn("teste");
+        when(rs.getBigDecimal("price")).thenReturn(new BigDecimal("1249.99"));
+        when(rs.getBoolean("available")).thenReturn(true);
+        when(rs.getBigDecimal("discount_percentage")).thenReturn(new BigDecimal("10.00"));
+        when(rs.getBigDecimal("final_price")).thenReturn(new BigDecimal("1124.99"));
+
+        when(rs.findColumn("created_at")).thenThrow(SQLClientInfoException.class);
+        when(rs.findColumn("category")).thenThrow(SQLClientInfoException.class);
 
         List<Product> resultList = productDao.findProductWithDiscount();
 
-        assertEquals(2, resultList.size());
-        assertEquals("camiseta seleção brasileira masculina - 2026", resultList.get(0).getName());
-        assertEquals(new BigDecimal("500.00"), resultList.get(0).getPrice());
-        assertEquals("teste", resultList.get(0).getDescription());
-        assertEquals("camiseta seleção brasileira feminina - 2026", resultList.get(1).getName());
-        assertEquals(new BigDecimal("500.00"), resultList.get(1).getPrice());
-        assertEquals("teste", resultList.get(0).getDescription());
+        assertEquals(1, resultList.size());
+        assertEquals("teste", resultList.get(0).getName());
+        assertEquals(new BigDecimal("1249.99"), resultList.get(0).getPrice());
+        assertEquals(new BigDecimal("1124.99"), resultList.get(0).getFinalPrice());
     }
 
     @Test
     void findByDate() throws SQLException {
 
         LocalDate date = LocalDate.of(2026, 7, 26);
+        PreparedStatement stmt = mock(PreparedStatement.class);
+        ResultSet rs = mock(ResultSet.class, withSettings().strictness(Strictness.LENIENT));
 
-        List<Product> productTest = List.of(new Product("iphone 17 pro max", new BigDecimal("7600"), 5, true, "teste", Category.TESTE));
+        when(connection.prepareStatement(anyString())).thenReturn(stmt);
+        when(stmt.executeQuery()).thenReturn(rs);
 
-        when(productDao.findProductsByDate(date)).thenReturn(productTest);
+        when(rs.findColumn("name")).thenReturn(1);
+        when(rs.findColumn("price")).thenReturn(2);
+        when(rs.findColumn("available")).thenReturn(3);
+        when(rs.findColumn("stock")).thenReturn(4);
+        when(rs.findColumn("discount_percentage")).thenReturn(5);
+        when(rs.findColumn("final_price")).thenReturn(6);
+        when(rs.findColumn("created_at")).thenReturn(7);
+
+        when(rs.next()).thenReturn(true).thenReturn(false);
+
+        when(rs.getString("name")).thenReturn("teste");
+        when(rs.getBigDecimal("price")).thenReturn(new BigDecimal("1249.99"));
+        when(rs.getBoolean("available")).thenReturn(true);
+        when(rs.getInt("stock")).thenReturn(10);
+        when(rs.getBigDecimal("discount_percentage")).thenReturn(new BigDecimal("10.00"));
+        when(rs.getBigDecimal("final_price")).thenReturn(new BigDecimal("1124.99"));
+        when(rs.getTimestamp("created_at")).thenReturn(Timestamp.valueOf(LocalDateTime.now()));
+
+        when(rs.findColumn("category")).thenThrow(SQLClientInfoException.class);
+        when(rs.findColumn("created_at")).thenThrow(SQLClientInfoException.class);
 
         List<Product> resultList = productDao.findProductsByDate(date);
 
         assertEquals(1, resultList.size());
-        assertEquals("iphone 17 pro max", resultList.get(0).getName());
-        assertEquals(new BigDecimal("7600"), resultList.get(0).getPrice());
-        assertEquals(5, resultList.get(0).getStock());
+        assertEquals("teste", resultList.get(0).getName());
+        assertEquals(new BigDecimal("1249.99"), resultList.get(0).getPrice());
+        assertEquals(10, resultList.get(0).getStock());
     }
 
     @Test
     void findSoldout() throws SQLException {
 
-        List<Product> productTest = List.of(new Product("televisao samsung oled 64' ", new BigDecimal("4299.99"), 0, false, "teste", Category.TESTE));
+        PreparedStatement stmt = mock(PreparedStatement.class);
+        ResultSet rs = mock(ResultSet.class, withSettings().strictness(Strictness.LENIENT));
 
-        when(productDao.findSoldOutProducts()).thenReturn(productTest);
+        when(connection.prepareStatement(anyString())).thenReturn(stmt);
+        when(stmt.executeQuery()).thenReturn(rs);
+
+        when(rs.findColumn("name")).thenReturn(1);
+        when(rs.findColumn("price")).thenReturn(2);
+        when(rs.findColumn("stock")).thenReturn(3);
+        when(rs.findColumn("discount_percentage")).thenReturn(4);
+        when(rs.findColumn("final_price")).thenReturn(5);
+
+        when(rs.next()).thenReturn(true).thenReturn(false);
+
+        when(rs.getString("name")).thenReturn("teste");
+        when(rs.getBigDecimal("price")).thenReturn(new BigDecimal("1249.99"));
+        when(rs.getInt("stock")).thenReturn(10);
+        when(rs.getBigDecimal("discount_percentage")).thenReturn(new BigDecimal("10.00"));
+        when(rs.getBigDecimal("final_price")).thenReturn(new BigDecimal("1124.99"));
+
+        when(rs.findColumn("category")).thenThrow(SQLClientInfoException.class);
 
         List<Product> resultList = productDao.findSoldOutProducts();
 
         assertEquals(1, resultList.size());
-        assertEquals("televisao samsung oled 64' ", resultList.get(0).getName());
+        assertEquals("teste", resultList.get(0).getName());
     }
 
     @Test
