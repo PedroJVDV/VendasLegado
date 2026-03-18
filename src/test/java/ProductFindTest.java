2e6dabffeb569ac16e33353aecf63a201c1f2ea3
Pedro <scrowzer.graal2@gmail.com>
2026-03-18 15:05:51 -0300

    new structure -- 100%

--- src/test/java/ProductFindTest.java
+++ src/test/java/ProductFindTest.java
@@ -27,7 +27,6 @@ public class ProductFindTest {
 
     private ProductDao productDao;
 
-
     @BeforeEach
     void beforeSetUp() {
         connection = mock(Connection.class);
@@ -218,6 +217,7 @@ public class ProductFindTest {
         when(rs.getBigDecimal("discount_percentage")).thenReturn(new BigDecimal("10.00"));
         when(rs.getBigDecimal("final_price")).thenReturn(new BigDecimal("1124.99"));
 
+        when(rs.findColumn("created_at")).thenThrow(SQLClientInfoException.class);
         when(rs.findColumn("category")).thenThrow(SQLClientInfoException.class);
 
         List<Product> resultList = productDao.findSoldOutProducts();
@@ -228,19 +228,40 @@ public class ProductFindTest {
 
     @Test
     void findByName() throws SQLException {
+        String name = "teste";
 
-        String name = "carrinho hotwheels limitado";
+        PreparedStatement stmt = mock(PreparedStatement.class);
+        ResultSet rs = mock(ResultSet.class, withSettings().strictness(Strictness.LENIENT));
 
-        List<Product> productTest = List.of(new Product("carrinho hotwheels limitado", new BigDecimal("400"), 2, true, "teste", Category.TESTE));
+        when(connection.prepareStatement(anyString())).thenReturn(stmt);
+        when(stmt.executeQuery()).thenReturn(rs);
 
-        when(productDao.findProductByName(name)).thenReturn(productTest);
+        when(rs.findColumn("name")).thenReturn(1);
+        when(rs.findColumn("price")).thenReturn(2);
+        when(rs.findColumn("category")).thenReturn(3);
+        when(rs.findColumn("available")).thenReturn(4);
+        when(rs.findColumn("stock")).thenReturn(5);
+        when(rs.findColumn("discount_percentage")).thenReturn(6);
+        when(rs.findColumn("final_price")).thenReturn(7);
+
+        when(rs.next()).thenReturn(true).thenReturn(false);
+
+        when(rs.getString("name")).thenReturn("teste");
+        when(rs.getBigDecimal("price")).thenReturn(new BigDecimal("1249.99"));
+        when(rs.getString("category")).thenReturn(Category.TESTE.name());
+        when(rs.getBoolean("available")).thenReturn(true);
+        when(rs.getInt("stock")).thenReturn(10);
+        when(rs.getBigDecimal("discount_percentage")).thenReturn(new BigDecimal("10.00"));
+        when(rs.getBigDecimal("final_price")).thenReturn(new BigDecimal("1124.99"));
+
+        when(rs.findColumn("created_at")).thenThrow(SQLClientInfoException.class);
 
         List<Product> resultList = productDao.findProductByName(name);
 
         assertEquals(1, resultList.size());
-        assertEquals(new BigDecimal("400"), resultList.get(0).getPrice());
-        assertEquals("carrinho hotwheels limitado", resultList.get(0).getName());
-        assertEquals(2, resultList.get(0).getStock());
+        assertEquals(new BigDecimal("1249.99"), resultList.get(0).getPrice());
+        assertEquals("teste", resultList.get(0).getName());
+        assertEquals(10, resultList.get(0).getStock());
         assertEquals(true, resultList.get(0).getAvailable());
     }
 
@@ -249,31 +270,62 @@ public class ProductFindTest {
 
         BigDecimal maxPrice = BigDecimal.valueOf(9700);
 
-        List<Product> productTest = List.of(
-                new Product("Placa de video RTX 5080", new BigDecimal("9700"), 1, true, "teste", Category.TESTE),
-                new Product("Placa de video RTX 2060", new BigDecimal("1900"), 1, true, "teste", Category.TESTE),
-                new Product("Placa de video RTX 3060", new BigDecimal("2300"), 1, true, "teste", Category.TESTE));
+        PreparedStatement stmt = mock(PreparedStatement.class);
+        ResultSet rs = mock(ResultSet.class, withSettings().strictness(Strictness.LENIENT));
 
-        when(productDao.findByMaxPrice(maxPrice)).thenReturn(productTest);
+        when(connection.prepareStatement(anyString())).thenReturn(stmt);
+        when(stmt.executeQuery()).thenReturn(rs);
+
+        when(rs.findColumn("name")).thenReturn(1);
+        when(rs.findColumn("price")).thenReturn(2);
+        when(rs.findColumn("available")).thenReturn(3);
+        when(rs.findColumn("category")).thenReturn(4);
+        when(rs.findColumn("stock")).thenReturn(5);
+        when(rs.findColumn("discount_percentage")).thenReturn(6);
+        when(rs.findColumn("final_price")).thenReturn(7);
+
+        when(rs.next())
+                .thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(false);
+
+        when(rs.getString("name"))
+                .thenReturn("teste")
+                .thenReturn("teste2");
+
+        when(rs.getBigDecimal("price"))
+                .thenReturn(new BigDecimal("3600"))
+                .thenReturn(new BigDecimal("5600"));
+
+        when(rs.getBoolean("available"))
+                .thenReturn(true)
+                .thenReturn(true);
+
+        when(rs.getString("category"))
+                .thenReturn(Category.TESTE.name())
+                .thenReturn(Category.TESTE.name());
+
+        when(rs.getInt("stock"))
+                .thenReturn(10)
+                .thenReturn(12);
+
+        when(rs.getBigDecimal("discount_percentage"))
+                .thenReturn(new BigDecimal("10.00"))
+                .thenReturn(new BigDecimal("10.00"));
+
+        when(rs.getBigDecimal("final_price"))
+                .thenReturn(new BigDecimal("1124.99"))
+                .thenReturn(new BigDecimal("1124.99"));
+
+        when(rs.findColumn("created_at")).thenThrow(SQLClientInfoException.class);
 
         List<Product> resultList = productDao.findByMaxPrice(maxPrice);
 
-
-        assertEquals(3, resultList.size());
-        assertEquals("Placa de video RTX 5080", resultList.get(0).getName());
-        assertEquals(new BigDecimal("9700"), resultList.get(0).getPrice());
-        assertEquals(1, resultList.get(0).getStock());
-        assertEquals(true, resultList.get(0).getAvailable());
-
-        assertEquals("Placa de video RTX 2060", resultList.get(1).getName());
-        assertEquals(new BigDecimal("1900"), resultList.get(1).getPrice());
-        assertEquals(1, resultList.get(1).getStock());
-        assertEquals(true, resultList.get(1).getAvailable());
-
-        assertEquals("Placa de video RTX 3060", resultList.get(2).getName());
-        assertEquals(new BigDecimal("2300"), resultList.get(2).getPrice());
-        assertEquals(1, resultList.get(2).getStock());
-        assertEquals(true, resultList.get(2).getAvailable());
+        assertEquals(2, resultList.size());
+        assertEquals("teste", resultList.get(0).getName());
+        assertEquals(new BigDecimal("3600"), resultList.get(0).getPrice());
+        assertEquals("teste2", resultList.get(1).getName());
+        assertEquals(new BigDecimal("5600"), resultList.get(1).getPrice());
     }
 
     @Test
@@ -282,40 +334,97 @@ public class ProductFindTest {
         BigDecimal maxPrice = BigDecimal.valueOf(5600);
         BigDecimal minPrice = BigDecimal.valueOf(1400);
 
-        List<Product> productTest = List.of(new Product("Mesa de tênis de mesa", new BigDecimal("2500"), 3, true, "teste", Category.TESTE),
-                new Product("Tênis Air Jordan", new BigDecimal("3500"), 2, true, "teste", Category.TESTE),
-                new Product("Skateboard", new BigDecimal("1400"), 3, true, "teste", Category.TESTE),
-                new Product("Notebook", new BigDecimal("5600"), 1, true, "teste", Category.TESTE),
-                new Product("Celular", new BigDecimal("4300"), 2, true, "teste", Category.TESTE));
+        PreparedStatement stmt = mock(PreparedStatement.class);
+        ResultSet rs = mock(ResultSet.class, withSettings().strictness(Strictness.LENIENT));
 
-        when(productDao.findPriceByRange(minPrice, maxPrice)).thenReturn(productTest);
+        when(connection.prepareStatement(anyString())).thenReturn(stmt);
+        when(stmt.executeQuery()).thenReturn(rs);
+
+        when(rs.findColumn("name")).thenReturn(1);
+        when(rs.findColumn("price")).thenReturn(2);
+        when(rs.findColumn("available")).thenReturn(3);
+        when(rs.findColumn("discount_percentage")).thenReturn(4);
+        when(rs.findColumn("final_price")).thenReturn(5);
+
+        when(rs.next())
+                .thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(false);
+
+        when(rs.getString("name"))
+                .thenReturn("teste")
+                .thenReturn("teste2")
+                .thenReturn("teste3")
+                .thenReturn("teste4")
+                .thenReturn("teste5");
+
+        when(rs.getBigDecimal("price"))
+                .thenReturn(new BigDecimal("3600"))
+                .thenReturn(new BigDecimal("5600"))
+                .thenReturn(new BigDecimal("4300"))
+                .thenReturn(new BigDecimal("4700"))
+                .thenReturn(new BigDecimal("5800"));
+
+        when(rs.getBoolean("available"))
+                .thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(true);
+
+        when(rs.getInt("stock"))
+                .thenReturn(10)
+                .thenReturn(12)
+                .thenReturn(14)
+                .thenReturn(16)
+                .thenReturn(18);
+
+        when(rs.getBigDecimal("discount_percentage"))
+                .thenReturn(new BigDecimal("10.00"))
+                .thenReturn(new BigDecimal("10.00"))
+                .thenReturn(new BigDecimal("10.00"))
+                .thenReturn(new BigDecimal("10.00"))
+                .thenReturn(new BigDecimal("10.00"));
+
+        when(rs.getBigDecimal("final_price"))
+                .thenReturn(new BigDecimal("1124.99"))
+                .thenReturn(new BigDecimal("1124.99"))
+                .thenReturn(new BigDecimal("1124.99"))
+                .thenReturn(new BigDecimal("1124.99"))
+                .thenReturn(new BigDecimal("1124.99"));
+
+        when(rs.findColumn("created_at")).thenThrow(SQLClientInfoException.class);
+        when(rs.findColumn("category")).thenThrow(SQLClientInfoException.class);
 
         List<Product> resultList = productDao.findPriceByRange(minPrice, maxPrice);
 
         assertEquals(5, resultList.size());
-        assertEquals("Mesa de tênis de mesa", resultList.get(0).getName());
-        assertEquals(new BigDecimal("2500"), resultList.get(0).getPrice());
-        assertEquals(3, resultList.get(0).getStock());
+        assertEquals("teste", resultList.get(0).getName());
+        assertEquals(new BigDecimal("3600"), resultList.get(0).getPrice());
+        assertEquals(10, resultList.get(0).getStock());
         assertEquals(true, resultList.get(0).getAvailable());
 
-        assertEquals("Tênis Air Jordan", resultList.get(1).getName());
-        assertEquals(new BigDecimal("3500"), resultList.get(1).getPrice());
-        assertEquals(2, resultList.get(1).getStock());
+        assertEquals("teste2", resultList.get(1).getName());
+        assertEquals(new BigDecimal("5600"), resultList.get(1).getPrice());
+        assertEquals(12, resultList.get(1).getStock());
         assertEquals(true, resultList.get(1).getAvailable());
 
-        assertEquals("Skateboard", resultList.get(2).getName());
-        assertEquals(new BigDecimal("1400"), resultList.get(2).getPrice());
-        assertEquals(3, resultList.get(2).getStock());
+        assertEquals("teste3", resultList.get(2).getName());
+        assertEquals(new BigDecimal("4300"), resultList.get(2).getPrice());
+        assertEquals(14, resultList.get(2).getStock());
         assertEquals(true, resultList.get(2).getAvailable());
 
-        assertEquals("Notebook", resultList.get(3).getName());
-        assertEquals(new BigDecimal("5600"), resultList.get(3).getPrice());
-        assertEquals(1, resultList.get(3).getStock());
+        assertEquals("teste4", resultList.get(3).getName());
+        assertEquals(new BigDecimal("4700"), resultList.get(3).getPrice());
+        assertEquals(16, resultList.get(3).getStock());
         assertEquals(true, resultList.get(3).getAvailable());
 
-        assertEquals("Celular", resultList.get(4).getName());
-        assertEquals(new BigDecimal("4300"), resultList.get(4).getPrice());
-        assertEquals(2, resultList.get(4).getStock());
+        assertEquals("teste5", resultList.get(4).getName());
+        assertEquals(new BigDecimal("5800"), resultList.get(4).getPrice());
+        assertEquals(18, resultList.get(4).getStock());
         assertEquals(true, resultList.get(4).getAvailable());
     }
 }
