62917b25a79ec10ab680ab536ab389efc7c52440
Pedro <scrowzer.graal2@gmail.com>
2026-03-24 17:08:09 -0300

    validate by priceRange and MaxPrice.. structure changes

--- src/main/java/com/pedrojvdv/jdbc/validation/ProductValidation.java
+++ src/main/java/com/pedrojvdv/jdbc/validation/ProductValidation.java
@@ -5,6 +5,8 @@ import com.pedrojvdv.jdbc.model.Category;
 import com.pedrojvdv.jdbc.model.DiscountType;
 import com.pedrojvdv.jdbc.model.Product;
 
+import java.math.BigDecimal;
+
 public class ProductValidation {
 
     public void validate(Product product){
@@ -15,12 +17,19 @@ public class ProductValidation {
         validateStock(product);
         validateAvailable(product);
     }
-    private void validateAvailable(Product product) {
+
+    public void validateId(Long id){
+        if (id == null || id <= 0){
+            throw new ProductValidationException("ID inválido!");
+        }
+    }
+
+    public void validateAvailable(Product product) {
        if (!product.getAvailable()){
            throw new ProductValidationException("Produto não está disponível!");
        }
     }
-    private void validateStock(Product product) {
+    public void validateStock(Product product) {
         if (product.getStock() > 300){
             throw new ProductValidationException("Stock não pode ultrapassar 300 unidades!");
         }
@@ -28,20 +37,30 @@ public class ProductValidation {
             throw new ProductValidationException("Stock acima de 100 unidades só é permitido em DESCONTOS PERMANENTES!");
         }
     }
-    private void validateDescription(String description) {
+    public void validateDescription(String description) {
         if (description == null || description.isBlank()){
             throw new ProductValidationException("Descrição do produto não pode ser nula/vazia!");
         }
     }
-    private void validateCategory(Category category) {
+    public void validateCategory(Category category) {
         if (category == null){
             throw new ProductValidationException("Categoria não pode ser nula/vazia!");
         }
     }
-    private void validateName(String name) {
+    public void validateName(String name) {
         if(name == null || name.isBlank()){
             throw new ProductValidationException("Nome não pode ser nulo/vazio");
         }
     }
 
+    public void validateByRange(BigDecimal minPrice, BigDecimal maxPrice){
+        if (minPrice == null || maxPrice == null){
+            throw new ProductValidationException("O valor minimo e máximo não pode ser nulo!");
+        }
+    }
+    public void validateMaxPrice(BigDecimal maxPrice){
+        if (maxPrice == null ){
+            throw new ProductValidationException("Preço máximo não pode ser nulo");
+        }
+    }
 }
