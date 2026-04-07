009b673c86e096e5fad63b764713eeb42d8f590a
Pedro <scrowzer.graal2@gmail.com>
2026-04-07 19:22:08 -0300

    create, update -> password query

--- src/main/java/com/pedrojvdv/jdbc/queries/user/UserCrudQueries.java
+++ src/main/java/com/pedrojvdv/jdbc/queries/user/UserCrudQueries.java
@@ -3,12 +3,12 @@ package com.pedrojvdv.jdbc.queries.user;
 public class UserCrudQueries {
 
     public static final String CREATE_USER = """
-            INSERT INTO user (name, email, age, created_at)
-            VALUES(?, ?, ?, CURRENT_TIMESTAMP)
+            INSERT INTO user (name, user_password, email, age, created_at)
+            VALUES(?, ?, ?, ? CURRENT_TIMESTAMP)
             """;
     public static final String UPDATE_USER = """
             UPDATE user
-            SET name = COALESCE(?, name), email = COALESCE(?, email), age = COALESCE(?, age)
+            SET name = COALESCE(?, name), user_password = COALESCE(?, user_password), email = COALESCE(?, email), age = COALESCE(?, age)
             WHERE id = ?
             """;
     public static final String DELETE_USER = """
