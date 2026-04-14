89af351d1d0bfb107c17f85603b0a715f5d9a42b
Pedro <scrowzer.graal2@gmail.com>
2026-04-14 19:40:32 -0300

    bugs fixed and new structure

--- src/main/java/com/pedrojvdv/jdbc/validation/LoginValidation.java
+++ src/main/java/com/pedrojvdv/jdbc/validation/LoginValidation.java
@@ -1,6 +1,7 @@
 package com.pedrojvdv.jdbc.validation;
 
 import com.pedrojvdv.jdbc.dao.UserDao;
+import com.pedrojvdv.jdbc.exception.UserException;
 import com.pedrojvdv.jdbc.model.User;
 import com.pedrojvdv.jdbc.model.enums.UserRole;
 import com.pedrojvdv.jdbc.util.PasswordEncoder;
@@ -10,29 +11,37 @@ import java.sql.SQLException;
 public class LoginValidation {
 
     private final UserDao userDao;
+    private final UserValidation userValidation;
 
-    public LoginValidation(UserDao userDao) {
-        this.userDao = userDao;
+    public LoginValidation() {
+        this.userDao = new UserDao();
+        this.userValidation = new UserValidation();
     }
 
-    public User authenticate (String email, String password)throws SQLException {
+    public User authenticate(String email, String password) throws SQLException {
+
         User user = userDao.findUserByEmail(email);
 
-        if (user == null){
+        if (user == null) {
             return null;
         }
-        if (!PasswordEncoder.match(password, user.getPassword())){
+
+        userValidation.validate(user);
+
+        if (!PasswordEncoder.match(password, user.getPassword())) {
             return null;
         }
         return user;
     }
 
-    public boolean isAdmin(User user){
-        return user != null && user.getRole() == UserRole.ADMIN;
+    public boolean isAdmin(User user) {
+        if (user.getPassword().equals("ADMIN#2026001")) {
+            return user.getRole() == UserRole.ADMIN;
+        }
+        return false;
     }
 
-    public boolean isUser(User user){
+    public boolean isUser(User user) {
         return user != null && user.getRole() == UserRole.USER;
     }
-    
 }
