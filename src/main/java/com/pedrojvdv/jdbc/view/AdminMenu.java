daad292b3dfc15371aa5a3e3587aa9c195b2bb2c
Pedro <scrowzer.graal2@gmail.com>
2026-05-20 19:58:19 -0300

    adminFunctions - view

---
+++ src/main/java/com/pedrojvdv/jdbc/view/AdminMenu.java
@@ -0,0 +1,73 @@
+package com.pedrojvdv.jdbc.view;
+
+import com.pedrojvdv.jdbc.model.User;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class AdminMenu {
+
+    private final ProductMenu productMenu;
+    private final Scanner scanner;
+    private final User user;
+
+    public AdminMenu(User user, Scanner scanner) {
+        this.user = user;
+        this.scanner = scanner;
+        this.productMenu = new ProductMenu(user,scanner);
+    }
+
+    public void showAdminMenu()throws SQLException{
+        boolean running = true;
+
+        while(running){
+            System.out.println(" || SISTEMA DE ADMINISTRADOR ||");
+            System.out.println("");
+
+            System.out.println("1 - ADMINISTRAÇÃO DE PRODUTOS");
+            System.out.println("2 - ADMINISTRAÇÃO DE DESCONTOS");
+
+            int choice = Integer.parseInt(scanner.nextLine());
+            ProductMenu productMenu = new ProductMenu(user, scanner);
+
+            switch (choice){
+                case 1:
+                   adminProductMenu();
+                   break;
+                case 7:
+                    running = false;
+                    break;
+            }
+        }
+    }
+
+    private void adminProductMenu() throws SQLException {
+        boolean running = true;
+
+        while (running) {
+            System.out.println("1 - CATALOGO DE PRODUTOS");
+            System.out.println("2 - BUSCAR PRODUTOS");
+            System.out.println("3 - GERENCIAR PRODUTOS");
+            System.out.println("4 - VOLTAR");
+            int choice = Integer.parseInt(scanner.nextLine());
+
+            switch (choice) {
+                case 1:
+                    productMenu.showCatalogMenu();
+                    break;
+                case 2:
+                    productMenu.showAdminSearchMenu();
+                    break;
+                case 3:
+                    productMenu.showManageMenu();
+                    break;
+                case 4:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Opção Inválida, tente novamente!");
+            }
+        }
+    }
+
+}
