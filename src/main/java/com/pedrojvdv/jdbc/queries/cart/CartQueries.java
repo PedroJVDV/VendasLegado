85e3b3f8116e3c330f8185900d5be5b417969d6b
Pedro <scrowzer.graal2@gmail.com>
2026-06-10 15:46:07 -0300

    cart find querie 0

--- src/main/java/com/pedrojvdv/jdbc/queries/cart/CartQueries.java
+++ src/main/java/com/pedrojvdv/jdbc/queries/cart/CartQueries.java
@@ -14,6 +14,21 @@ public class CartQueries {
             DELETE FROM cart
             WHERE user_id = ?
             """;
+
+    public static final String FIND_CART_PRODUCTS = """
+            SELECT
+                c.quantity,
+                p.name AS product_name,
+                p.price AS original_price,
+                d.percentage AS product_discount,
+                p.price * (1 - COALESCE(d.percentage, 0) / 100) AS final_price,
+                d.type AS discount_type
+            FROM cart c
+            LEFT JOIN products p
+                ON c.product_id = p.id
+            LEFT JOIN discount d
+                ON p.id = d.product_id
+            """;
     public static final String FIND_CART_USERID = """
             SELECT
                 id,
